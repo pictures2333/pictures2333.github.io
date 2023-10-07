4bc4b5761a2ed69081c23b281c86e9a0ec578d83
pictures2333 <m0987529191@gmail.com>
2023-10-07 19:24:34 +0800

    mobile view update

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ var scrolled = false;
 
 window.onscroll = () => {
     const scrollPositionY = window.scrollY;
-    var hw = 0.1;
-    if (scrollPositionY >= window.innerHeight*hw && scrolled == false){
+    if (scrollPositionY >= window.innerHeight*0.15 && scrolled == false){
         var introbox = document.querySelector(".introbox");
         introbox.style.animationFillMode = "forwards";
         introbox.style.animation = "boxinto 1s forwards";
         scrolled = true;
-    }else if (scrollPositionY < window.innerHeight*hw && scrolled == true){
+    }else if (scrollPositionY < window.innerHeight*0.05 && scrolled == true){
         var introbox = document.querySelector(".introbox");
         introbox.style.animationFillMode = "forwards";
         introbox.style.animation = "boxout 1s forwards";
